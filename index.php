cc4e5bc395b4a866803ab54e2e1ab309f0d193c1
Vladimir V. Myagdeev <concertbil@gmail.com>
2017-06-15 20:50:58 +0500

    PHP https test

---
+++ index.php
@@ -0,0 +1,8 @@
+$_SERVER['HTTPS']: <?=empty($_SERVER['HTTPS']) ? 'empty' : $_SERVER['HTTPS']; ?><br />
+Valid: On or 1<br />
+<br />
+<br />
+$_SERVER['SERVER_PORT']: <?=$_SERVER['SERVER_PORT'];?><br />
+Default ports:<br />
+http: 80<br />
+https: 443<br />
