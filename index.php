55514eebf271d202357f661632ed397b0feb39a3
Vladimir Myagdeev <git@fest.one>
2017-06-16 17:36:13 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<html>
+<head>
+    <title>PHP https test</title>
+</head>
+<body>
 $_SERVER['HTTPS']: <?=empty($_SERVER['HTTPS']) ? 'empty' : $_SERVER['HTTPS']; ?><br />
 Valid: On or 1<br />
 <br />
@@ -6,3 +11,7 @@ $_SERVER['SERVER_PORT']: <?=$_SERVER['SERVER_PORT'];?><br />
 Default ports:<br />
 http: 80<br />
 https: 443<br />
+<br />
+<a href="https://github.com/vvmspace/php-https-test">PHP https test</a> © Vladimir V. Myagdeev
+</body>
+</html>
